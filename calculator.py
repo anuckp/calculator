b61d9e04c466b2cd976a419492bf1b293c58c013
anuckp <anu.ckp.13@gmail.com>
2019-07-02 01:30:22 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,22 +1,30 @@
-x=int(input("enter the first num"))
-y=int(input("enter the second num"))
-z=input("enter the operator")
+x=int(input("Enter the first num: "))
+y=int(input("Enter the second num: "))
+while True:
+    z=input("Enter the operator: ")
+    if z=='+':
+        result=x+y
+        print(result)
+        break
 
-if z=='+':
-    result=x+y
-    print(result)
+    elif z=='-':
+        result=x-y
+        print(result)
+        break
     
-elif z=='-':
-    result=x-y
-    print(result)
+    elif z=='*':
+        result=x*y
+        print(result)
+        break
     
-elif z=='*':
-    result=x*y
-    print(result)
-    
-elif z=='/':
-    result=x/y
-    
-else:
-    print("choose in between + - * /")
+    elif z=='/':
+        result=x/y
+        print(result)
+        break
+    elif z=='%':
+        result=x%y
+        print(result)
+        break
+    else:
+        print("choose in between + - * /")
     
