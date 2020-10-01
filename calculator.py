77bbe5d18cc6bd6aada819a78e69a97ab56c6de1
Utkarsh Mall <utkarshmall247@gmail.com>
2020-10-01 19:02:41 +0530

    Update calculator.py
    Added a function for performing calculations and make the task faster and easier, also added a docstring for the function.

--- calculator.py
+++ calculator.py
@@ -1,30 +1,42 @@
-x=int(input("Enter the first num: "))
-y=int(input("Enter the second num: "))
-while True:
+def calculator():
+    ''' This is a function which when called do the calculation based operations which user wants '''
+
+    x=int(input("Enter the first num: "))
+    y=int(input("Enter the second num: "))
+
     z=input("Enter the operator: ")
+
     if z=='+':
         result=x+y
         print(result)
-        break
 
     elif z=='-':
         result=x-y
         print(result)
-        break
-    
+        
     elif z=='*':
         result=x*y
         print(result)
-        break
-    
+        
     elif z=='/':
         result=x/y
         print(result)
-        break
+       
     elif z=='%':
         result=x%y
         print(result)
-        break
+
+    elif z=='**':
+        result=x**y
+        print(result)
+        
     else:
-        print("choose in between + - * /")
-    
+        print("Choose in between +,  -,  *,  /, **")
+        
+
+while True:
+    i = int(input("Press 1 to Continue, otherwise press 0\n")) 
+    if i == 1:
+        calculator()
+    else:
+        break
